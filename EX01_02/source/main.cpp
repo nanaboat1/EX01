f0acd68ad7499c7489e3e72086c84a8c0d53e7a3
namoah23@my.whitworth.edu <namoah23@my.whitworth.edu>
2020-09-13 11:29:32 -0700

    Initial Commit

---
+++ EX01_02/source/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // the standard input and output library
+
+
+/**
+ * @param a length of one side of triangle
+ * @param b length of the second side of the triangle
+ * @param c length of the third side of the triangle 
+ * @return either the sides maeup a triangle or not
+ */
+
+
+
+
+int main( ) {
+
+
+
+
+
+    return 0; // return's upon completion
+}
+
